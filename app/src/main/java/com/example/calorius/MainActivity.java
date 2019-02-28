51dd53cb6774f1dd7be3dd21835b9b059de97908
Inakisu <izorrilla008@ikasle.ehu.eus>
2019-02-28 21:14:22 +0100

    Creada clase MainFragment (como prueba, la usaremos como login), y un fragment_main.xml que se verá al clicar en login en el drawer

--- app/src/main/java/com/example/calorius/MainActivity.java
+++ app/src/main/java/com/example/calorius/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calorius;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -83,6 +84,9 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_login) {
             // Lo que pasa cuando pulsas en login
+            MainFragment mainFragment = new MainFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.frameLayout, mainFragment).commit();
 
         } else if (id == R.id.nav_logout) { //Lo que pasa cuando pulsas en logout
 
