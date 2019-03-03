8ed5090fbcf2b3e10147d2761f7eba798b80fbf9
Iñaki Zorrilla García <izorrilla008@ikasle.ehu.eus>
2019-03-03 19:06:04 +0100

    fragmento registro calorías. Duda cómo añadir alimentos? Calorías? Cantidad?

---
+++ app/src/main/java/com/example/calorius/regCalFragment.java
@@ -0,0 +1,29 @@
+package com.example.calorius;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class regCalFragment extends Fragment {
+
+
+    public regCalFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reg_cal, container, false);
+    }
+
+}
