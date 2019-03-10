460c7cd339ba6351570af665b80ce11ba6bcfac4
Iñaki Zorrilla García <izorrilla008@ikasle.ehu.eus>
2019-03-10 17:57:06 +0100

    AHORA EN develop VAMOS A TRABAJAR CON HTTPCLIENT, al carajo el httpurlconnection

--- app/src/main/java/com/example/calorius/LoginFragment.java
+++ app/src/main/java/com/example/calorius/LoginFragment.java
@@ -1,6 +1,7 @@
 package com.example.calorius;
 
 
+import android.annotation.TargetApi;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -25,6 +26,11 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.methods.HttpGet;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import cz.msebera.android.httpclient.util.EntityUtils;
 
 
 /**
@@ -41,7 +47,6 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -54,6 +59,7 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
         botonLogin = (Button) v.findViewById(R.id.loginButton);
         botonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
+            @TargetApi(11)
             public void onClick(View v) {
                 TareaWSObtener tareaAsincrona = new TareaWSObtener();
 
@@ -71,74 +77,52 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
 
 
     //A partir de aquí pasan cosas de HTTP REST
-
+    @TargetApi(11)
     private class TareaWSObtener extends AsyncTask<String,Integer,Boolean> {
 
-        private String email;
-        private String password;
+    private String fotoUsu = "No se ha provisto";
 
         protected Boolean doInBackground(String... params) {
 
             boolean resul = true;
-            String texto = params[0];
 
-            String url = "http://192.168.0.24:567/Api/Usuarios/Usuario/"+params[0]+"/"; //esto tiene que concretarse
+            HttpClient httpClient = new DefaultHttpClient();
 
-            try { //Comenzamos creando la conexion HTTPURL
-                URL objUrl = new URL("http://192.168.0.24:567/Api/Usuarios/Usuario/"+params[0]+"/");
-                HttpURLConnection urlConnection = (HttpURLConnection) objUrl.openConnection();
-                urlConnection.setDoOutput(true);
-                urlConnection.setDoInput(true);
-                urlConnection.setUseCaches(false);
-                urlConnection.setRequestProperty("Accept-Language", "ES");
-                urlConnection.setRequestProperty("Content-Type", "application/json");
-                urlConnection.setRequestProperty("Accept", "application/json");
-                urlConnection.setRequestMethod("GET"); //Operación GET
-                urlConnection.connect();
-                //Obtenemos códigos de respuesta HTTP para saber si hay errores o no
-                int responseCode = urlConnection.getResponseCode();
-                String responseMessage = urlConnection.getResponseMessage();
+            String email = params[0];
+            System.out.println("Email escrito: "+ email);
+            HttpGet del =
+                    new HttpGet("http://192.168.0.24:567/Api/Usuarios/Usuario/" + email+"/");
 
-                System.out.println("--> responseCode es: "+ responseCode);
-                System.out.println("--> responseMensage es: "+ responseMessage);
+            del.setHeader("content-type", "application/json");
 
-                if (responseCode == HttpURLConnection.HTTP_OK){
-                    System.out.println("Hurra! Error 200! HTTP OK!");
-
-                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                    //InputStreamReader inReader = new InputStreamReader(in, "UTF-8");
-
-                    BufferedReader reader = new BufferedReader
-                            (new InputStreamReader(urlConnection.getInputStream()));
-
-                    String line;
-                    StringBuffer response = new StringBuffer();
-                    while ((line = reader.readLine())!=null){
-                        response.append(line);
-                    }
-
-                    JSONObject datoObtenido = new JSONObject(response.toString()); //Construimos el objeto Usuario en formato JSON
-                    String email = datoObtenido.getString("email");
-                    String passwd = datoObtenido.getString("password");
-                }else{
-                    System.out.println("Error HTTP:");
-                    System.out.println("--> responseCode es: "+ responseCode);
-                    System.out.println("--> responseMensage es: "+ responseMessage);
-                }
-            } catch(Exception ex)
+            try
             {
-                Log.e("ServicioRest", "Error!", ex);
-                resul = false;
+                HttpResponse resp = httpClient.execute(del);
+                String respStr = EntityUtils.toString(resp.getEntity());
+
+                JSONObject respJSON = new JSONObject(respStr);
+
+                String emailUsu = respJSON.getString("email");
+                String passwdUsu = respJSON.getString("password");
+                fotoUsu = respJSON.getString("foto");
+
+                System.out.println("Devuelve: " + emailUsu + " - " + passwdUsu + " - " + fotoUsu);
+                //lblResultado.setText("" + emailUsu + " - " + passwdUsu + " - " + fotoUsu);
+
+            }
+            catch(Exception ex)
+            {
+                Log.e("ServicioRest","Error!", ex);
             }
 
             return resul;
         }
 
-        protected void onPostExecute(Boolean result) { //A partir de aquí hacer que funcione
+        protected void onPostExecute(Boolean result) {
 
             if (result)
             {
-              //lblResultado.setText("-> " + email + " - " + password);
+              //  lblResultado.setText("" + idCli + "-" + nombCli + "-" + telefCli);
             }
         }
     }
